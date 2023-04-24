8fe5c07b33fa5b19d4898231bd5494dfe5b71e05
Patrick Stern <stern-patrick@web.de>
2023-04-24 11:35:39 +0200

    [UPDATE] webpack config // css-modules localIdentName

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src/styles')],
+        prependData: `@import "_utils.scss";`
+    },
+
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		config.module.rules.forEach(({ oneOf }) =>
+			oneOf?.forEach(({ use }) => {
+				use?.forEach?.(({ options}) => {
+                    localIdentName = dev ? '[name]_[local]__[hash:base64:5]' : 'css__[hash:base64:4]';
+				});
+			}),
+		);
+
+		return config;
+	},
 }
 
 module.exports = nextConfig
